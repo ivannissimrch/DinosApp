e6661dbe1beb1dbbbbef448f94039e70be3bec68
Ivan Rebolledo <ivannissimrch@gmail.com>
2022-10-15 08:38:29 -0400

    remove unused code on server

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 /* Require Express to run server and routes */
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const port = 3000;
 
 /* Start up an instance of app */
 const app = express();
 
-/* Middleware
-Here we are configuring express to use body-parser as middle-ware. */
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-/* Cors for cross origin allowance */
-app.use(cors());
-/* Initialize the main project folder*/
 app.use(express.static("DinoApp"));
 
 /* Setup Server */
